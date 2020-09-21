e3651b4e440d5a3585347ecd9a3a7bee525e7063
jina <psalvarajah@lcs.on.ca>
2020-09-21 12:10:15 -0400

    Created navigationLinkContent

---
+++ FavouriteThings/NavigationLinkContent.swift
@@ -0,0 +1,46 @@
+//
+//  NavigationLinkContent.swift
+//  FavouriteThings
+//
+//  Created by Salvarajah, Prajina on 2020-09-21.
+//  Copyright © 2020 Salvarajah, Prajina. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationLinkContent: View {
+    
+    //Define porperties
+    var imageName: String
+    var mainText: String
+    var subText: String
+    
+    var body: some View {
+       
+        HStack {
+            
+            Image(imageName)
+                .resizable()
+                .frame(width: 100.0, height: 80.0)
+            
+            
+            VStack(alignment: .leading) {
+                
+                //Provide the label for the navigation link
+                Text(mainText)
+                    .font(.headline)
+                
+                Text(subText)
+                    .font(.caption)
+                
+        }
+            
+        }
+      }
+}
+
+struct NavigationLinkContent_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationLinkContent(imageName:"bus", mainText:"I'm passionate about...", subText:"Better transportation")
+    }
+}
